6afc8ad5f982ea78f52f25d8313566bcb18497cd
Ashwin Singh <45795129+90r1ll4@users.noreply.github.com>
2023-03-26 14:57:59 +0530

    Add files via upload

---
+++ gotldwizard/cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var version = "0.0.1"
+
+var rootCmd = &cobra.Command{
+	Use:     "tldwizard",
+	Short:   "Changing the TLD for a given domain name",
+	Long:    ` A TLD is the part of a domain name that comes after the final dot, such as .com, .org, .net, or .edu. By swapping out the TLD, you can modify the domain name's extension to better suit your needs.	`,
+	Version: version,
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
+}
+
+func init() {
+	rootCmd.AddCommand()
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
